0793d816a3f3e722fb6e48f7c913c183446a862c
xuelang <1045578710@qq.com>
2019-09-23 21:23:34 +0800

    add gitignore file at 201909232123

---
+++ runtime/cache/71/52bf241e678272f31e80544a9ef46c.php
@@ -0,0 +1,4 @@
+<?php
+//000000000000
+ exit();?>
+a:5:{s:2:"id";a:7:{s:4:"name";s:2:"id";s:4:"type";s:16:"int(10) unsigned";s:7:"notnull";b:0;s:7:"default";N;s:7:"primary";b:1;s:7:"autoinc";b:1;s:7:"comment";s:14:"身份管理id";}s:9:"role_name";a:7:{s:4:"name";s:9:"role_name";s:4:"type";s:11:"varchar(32)";s:7:"notnull";b:0;s:7:"default";N;s:7:"primary";b:0;s:7:"autoinc";b:0;s:7:"comment";s:18:"身份管理名称";}s:5:"rules";a:7:{s:4:"name";s:5:"rules";s:4:"type";s:4:"text";s:7:"notnull";b:0;s:7:"default";N;s:7:"primary";b:0;s:7:"autoinc";b:0;s:7:"comment";s:28:"身份管理权限(menus_id)";}s:5:"level";a:7:{s:4:"name";s:5:"level";s:4:"type";s:19:"tinyint(3) unsigned";s:7:"notnull";b:0;s:7:"default";s:1:"0";s:7:"primary";b:0;s:7:"autoinc";b:0;s:7:"comment";s:0:"";}s:6:"status";a:7:{s:4:"name";s:6:"status";s:4:"type";s:19:"tinyint(1) unsigned";s:7:"notnull";b:0;s:7:"default";s:1:"1";s:7:"primary";b:0;s:7:"autoinc";b:0;s:7:"comment";s:6:"状态";}}
---
+++ runtime/session/sess_7cd69a5670f790c03ec27a6a4cba1c04.php
@@ -0,0 +1,4 @@
+<?php
+//000000000000
+ exit();?>
+a:4:{s:11:"login_error";N;s:3:"pid";i:0;s:7:"adminId";i:1;s:9:"adminInfo";a:12:{s:2:"id";i:1;s:7:"account";s:5:"admin";s:3:"pwd";s:32:"3f9b301e64e6447fd33696d19cb23661";s:9:"real_name";s:5:"admin";s:5:"roles";s:1:"1";s:7:"last_ip";s:9:"127.0.0.1";s:9:"last_time";i:1569244345;s:8:"add_time";i:1569243569;s:11:"login_count";i:0;s:5:"level";i:0;s:6:"status";i:1;s:6:"is_del";i:0;}}
